5901eec0dcdad72977eac5be432446e8341ce480
poaassasin <109660255+poaassasin@users.noreply.github.com>
2024-03-01 20:50:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $nim = "225150407111008";
+        $nama = "Muhammad Azka Ayubi";
+
+        for ($i = 1; $i <= 5; $i++) {
+            echo "<p>Iterasi ke-$i:</p>";
+            echo "<p>NIM: $nim</p>";
+            echo "<p>Nama: $nama</p>";
+            echo "<hr>";
+        }
+        ?>
+    </body>
+</html>
